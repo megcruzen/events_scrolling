32843344fb937bddd61f2eb07a5c53ff562ec780
Meg Cruzen <megcruzen@gmail.com>
2018-12-04 13:29:01 -0600

    Updated js file and finished exercise

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
-const audrey = document.getElementById("audrey")
+const audrey = document.getElementById("audrey");
 
 /*
     Add an event listener to the `document` object to listen
     for the "scroll" event.
 */
 
-.addEventListener("scroll", function () {
+window.addEventListener("scroll", function () {
     /*
         Adjust the width of audrey to be 1/3 the value of
         `window.scrollY`. No lower than 50px, though.
     */
 
+    let size = window.scrollY;
+    let width = size / 3;
+    audrey.style.width = `${width}px`;
+    audrey.style.minWidth = "50px";
 
     /*
         Adjust the height of audrey to be 1/4 the value of
         `window.scrollY`. No lower than 100px, though.
     */
-})
+
+    let height = size / 4;
+    audrey.style.height = `${height}px`;
+    audrey.style.minHeight = "100px";
+});
